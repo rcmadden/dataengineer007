3137b312a279d0e33a95960185847d8977d58a69
Russia Madden <dataengineer007@gmail.com>
2016-05-26 10:28:18 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+# TODO: change to use url_for
+app = Flask(__name__, static_url_path="/static", static_folder="static")
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True  # TODO: Set to false on deployment
+    app.run()
