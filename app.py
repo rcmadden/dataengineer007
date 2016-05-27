b2b1f5442b9d7abeb86aadf6b597e52f95d31022
Russia Madden <dataengineer007@gmail.com>
2016-05-27 00:14:42 +0000

    add bootstrap theme

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 # TODO: change to use url_for
 app = Flask(__name__, static_url_path="/static", static_folder="static")
 #app = Flask(__name__)
@@ -7,8 +8,8 @@ app = Flask(__name__, static_url_path="/static", static_folder="static")
 def hello_world():
     return render_template('index.html')
 
-#if __name__ == '__main__':
-#    app.debug = True  # TODO: Set to false on deployment
-#:    app.run()
+app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 
-
+if __name__ == "__main__":
+    app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+    app.run(debug=True)
