a9c1eceb5c2e2aa69e92ea2ec1249237accd1153
Russia Madden <dataengineer007@gmail.com>
2016-05-30 15:26:50 +0000

    comment out to run in prod

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__, static_url_path="/static", static_folder="static")
 def hello_world():
     return render_template('index.html')
 
-app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+#app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 
-if __name__ == "__main__":
-    app.config['TRAP_BAD_REQUEST_ERRORS'] = True
-    app.run(debug=True)
+#if __name__ == "__main__":
+ #   app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+ #   app.run(debug=True)
